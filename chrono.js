975c73ef2aac19fbbc37df3f878cd89d60d93cc1
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-10 16:45:12 +0100

    fonctionnement du bouton start

--- chrono.js
+++ chrono.js
@@ -9,13 +9,32 @@ var ms=0,s=0,mn=0,h=0;
 
 function start(){
 t =setInterval(update_chrono,100);
-btn_start.disabled=true;
+btn_start=true;
 
 }
 
     /*La fonction update_chrono incrémente le nombre de millisecondes par 1 <==> 1*cadence = 100 */
 function update_chrono(){
 ms+=1;
+    /*si ms=10 <==> ms*cadence = 1000ms <==> 1s alors on incrémente le nombre de secondes*/
+    if(ms==10){
+        ms=1;
+        s+=1;
+       }
+       /*on teste si s=60 pour incrémenter le nombre de minute*/
+       if(s==60){
+        s=0;
+        mn+=1;
+       }
+       if(mn==60){
+        mn=0;
+        h+=1;
+       }
+       /*afficher les nouvelle valeurs*/
+       sp[0].innerHTML=h+" h";
+       sp[1].innerHTML=mn+" min";
+       sp[2].innerHTML=s+" s";
+       sp[3].innerHTML=ms+" ms";
+//Source : www.exelib.net
   }
 
-   console.log(start)
